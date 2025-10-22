a12d5c08ae5b1366468730e5b7e9a96a65502dd1
LeeJaein <devxoox1106@gmail.com>
2025-10-23 01:08:57 +0900

    feat: final

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const Box = (props) => {
+    
+    let result;
+    if(props.title === "Computer" && props.result !== "TIE" && props.result !== "" ){
+        result = (props.result === "WIN") ? "LOSE" : "WIN";
+    }else{
+        result = props.result;
+    }
+
   return (
-    <div className='box'> 
+    <div className={`box ${result}`}> 
       <h1>{props.title}</h1>
-      {props.item ? (<img className='item-img' src={props.item.img}></img>) : (<div className='item-img'></div>) }
-      <h2>WIN</h2>
+      {props.item ? (<img className='item-img' src={props.item.img}></img>) : (<img className='item-img' src='/image/question.png'></img>) }
+      <h2>{result}</h2>
     </div>
   )
 }
